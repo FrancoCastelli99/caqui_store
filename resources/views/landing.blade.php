f163a337e30bd4500953f94cba7c3fff8bfeba47
Popo <72705327+FrancoCastelli99@users.noreply.github.com>
2021-05-11 23:00:19 -0400

    [advance] visual landing

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,32 @@
+@extends('base')
+
+@section('title')
+    CaquiStore | Home
+@endsection
+
+@section('content')
+    <section>
+        <h2 class="text-center font-merriweather">ENVIOS A TODO CHILE</h1>
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="{{asset('img/delivery-ico.png')}}" alt="" style="width: inherit; height: auto; margin-top: -135px;">
+                </div>
+                <div class="col-md-6">
+                    <p class="font-merriweather">Realizamos envios a todo chile por pagar en Starken y Chilexpress dias
+                        Martes y Viernes. Haz tu compra antes de las 18 hrs del dia anterior para que tu pedido sea agendado y salga al dia siguiente.
+                    </p>
+                </div>
+    
+                <div class="col-md-3">
+                    
+                </div>
+
+
+            </div>
+            
+        </div>
+    </section>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing');
+});
+Route::get('/personaliza', function () {
+    return view('base');
 });
